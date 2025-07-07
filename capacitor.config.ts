7cb31834f4463ea9c2aab403c1d8522f5ca59ae2
emna-charmi2 <emnacharmi87@gmail.com>
2025-07-07 16:13:54 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nutritionApp1',
+  webDir: 'www'
+};
+
+export default config;
